8080ba14cc6c0a31d680a642392080992d920302
Suman-Thapa <sumanthapa302702@gmail.com>
2025-11-22 13:01:23 +0545

    adding connection.php in feature1

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+    $con = mysqli_connect("localhost","suman","suman123", "HotelBooking");
+    if(!$con){
+        die("Can not connect");
+    }
+
+    echo "Database Connected Sucessfully";
+
+    ?>
+
